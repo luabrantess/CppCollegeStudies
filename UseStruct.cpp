f668d6885cd54f6626cf2da3ce7dea65b4603420
luabrantess <analuizaabr4@gail.com>
2025-04-10 10:51:43 -0300

    Changing Gitingore and adding a new struct

--- UseStruct.cpp
+++ UseStruct.cpp
@@ -16,9 +16,21 @@ struct Book{
 
 };
 
+typedef struct {
+    int x;
+    int y;
+
+} Ponto;
+
 int main(){
 
     Book b1("A culpa é das estrelas", 300, "Fulano");
     std::cout << "O nome é " << b1.name <<std::endl ;
     std::cout << "As páginas são " << b1.pages << std::endl;
+
+    Ponto p1;
+    p1.x = 1;
+    p1.y = 2;
+
+    std::cout << "P1 é " << p1.x << " e " << p1.y << std::endl;
 }
